b39a78349c712ac1c7be8fc837abab1385d7e167
tty66@mail.ru <tty66@mail.ru>
2021-07-28 18:47:27 +0300

    table defined

--- main_hub/conf_table.py
+++ main_hub/conf_table.py
@@ -1,4 +1,16 @@
 HUB_TEST = 0
+HUB_1 = 1
+HUB_2 = 2
+HUB_3 = 3
+HUB_4 = 4
+HUB_5 = 5
+HUB_6 = 6
+HUB_7 = 7
+HUB_8 = 8
+HUB_9 = 9
+HUB_10 = 10
+HUB_11 = 11
+HUB_12 = 12
 HUB_MAIN = 999
 
 hubs_conf_table = {
@@ -6,6 +18,54 @@ hubs_conf_table = {
             'Q': 0.26,   
             'set_task_time': 0
         },
+    HUB_1 : {
+            'Q': 0.26,   
+            'set_task_time': 0
+        },
+    HUB_2 : {
+            'Q': 0.85,   
+            'set_task_time': 0
+            },
+    HUB_3 : {
+            'Q': 0.39,   
+            'set_task_time': 0
+            },
+    HUB_4 : {
+            'Q': 0.09,   
+            'set_task_time': 0
+            },
+    HUB_5 : {
+            'Q': 0.06,   
+            'set_task_time': 0
+            },
+    HUB_6 : {
+            'Q': 0.10,   
+            'set_task_time': 0
+            },
+    HUB_7 : {
+            'Q': 0.14,   
+            'set_task_time': 0
+            },
+    HUB_8 : {
+            'Q': 0.45,   
+            'set_task_time': 0
+            },
+    HUB_9 : {
+            'Q': 0.30,   
+            'set_task_time': 0
+            },
+    HUB_10 : {
+            'Q': 0.06,   
+            'set_task_time': 0
+            },
+    HUB_11 : {
+            'Q': 0.27,   
+            'set_task_time': 0
+            },
+    HUB_12 : {
+            'Q': 0.35,   
+            'set_task_time': 0
+            },
     HUB_MAIN : {
             'vanadium': 0
         }
